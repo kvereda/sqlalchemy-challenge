b844faad48a17a3a52e4975a5d63401b173ce8f7
KESHIA VEREDA <vereda761@gmail.com>
2020-08-10 18:44:40 -0700

    app.py

---
+++ app.py
@@ -0,0 +1,54 @@
+# 1. import Flask
+import numpy as np
+
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+import datetime as dt
+
+# Database setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# Reflect an existing database into a new model
+Base = automap_base()
+
+# Reflect tables
+Base.prepare(engine, reflect=True)
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+# Save references to each table
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+# Last data point
+last_data_point = session.query(measurement.date).order_by(measurement.date.desc()).first()
+
+
+
+# 2. Create an app, being sure to pass __name__
+app = Flask(__name__)
+
+
+# 3. Define what to do when a user hits the index route
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return "Welcome to my 'Home' page!"
+
+
+# 4. Define what to do when a user hits the /about route
+@app.route("/about")
+def about():
+    print("Server received request for 'About' page...")
+    return "Welcome to my 'About' page!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
